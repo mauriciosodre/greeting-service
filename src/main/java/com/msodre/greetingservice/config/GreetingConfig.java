423d17ca97b3b7f089bf16da1680f55cd6971abb
msodrejr <m.sodre8@gmail.com>
2022-03-25 00:35:36 -0300

    Refresh das configs sem parar o service

--- src/main/java/com/msodre/greetingservice/config/GreetingConfig.java
+++ src/main/java/com/msodre/greetingservice/config/GreetingConfig.java
@@ -3,11 +3,13 @@ package com.msodre.greetingservice.config;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @NoArgsConstructor
 @Data
 @Component
+@RefreshScope
 @ConfigurationProperties("greeting-service")
 public class GreetingConfig {
 
